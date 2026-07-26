7bab11a3d174e6fbe18e206efae81d3ecf93d8f8
David Caress <caress@mbari.org>
2026-07-25 22:32:41 -0700

    Prep for release 5.8.3beta16
    Release 5.8.3beta15 was botched due to a faulty merge of conflicting pull requests. Hopefully this fixes things...

--- src/mbio/mb_define.h
+++ src/mbio/mb_define.h
@@ -36,8 +36,8 @@
 #include <stdint.h>
 
 /* Define version and date for this release */
-#define MB_VERSION "5.8.3beta15"
-#define MB_VERSION_DATE "25 July 2026"
+#define MB_VERSION "5.8.3beta16"
+#define MB_VERSION_DATE "26 July 2026"
 
 /* CMake supports current OS's and so there is only one form of RPC and XDR and no mb_config.h file */
 #ifdef CMAKE_BUILD_SYSTEM
